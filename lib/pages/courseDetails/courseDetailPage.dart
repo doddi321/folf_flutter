fc0934332c76122f67bf8c7cf67d79fa5be06b5d
Þórður Friðriksson <thordurf17@ru.is>
2019-06-30 12:28:34 +0000

    man ekki

--- lib/pages/courseDetails/courseDetailPage.dart
+++ lib/pages/courseDetails/courseDetailPage.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:folf/constants/myColors.dart';
+import 'package:folf/pages/courseDetails/BodySelectCourse2.dart';
 
+import 'BodySelectCourse.dart';
 import 'bodyDetails/bodyDetails.dart';
 import 'headDetails.dart';
 
-class CourseDetailPage extends StatelessWidget {
+class CourseDetailPage extends StatefulWidget {
+  @override
+  _CourseDetailPageState createState() => _CourseDetailPageState();
+}
+
+class _CourseDetailPageState extends State<CourseDetailPage> {
+  static const int _details = 0;
+  static const int _selectCourse = 1;
+  int _stage = 0;
+
   @override
   Widget build(BuildContext context) {
+    Widget body = _getBody();
+
     return Container(
         child: Scaffold(
-            bottomSheet: Material(
-              color: MyColors.courseDetailOrange,
-              child: InkWell(
-                onTap: () {},
-                child: Container(
-                  height: 60,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "HEFJA LEIK",
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            bottomSheet: _buildStartButton(),
             appBar: AppBar(
               title: Text('Guðmundarlundur'),
             ),
@@ -36,9 +32,44 @@ class CourseDetailPage extends StatelessWidget {
                 children: <Widget>[
                   HeadDetails(),
                   SizedBox(height: 5.0),
-                  BodyDetails()
+                  body,
                 ],
               ),
             )));
+  }
+
+  Widget _getBody() {
+    switch (_stage) {
+      case _details:
+        return BodyDetails();
+      case _selectCourse:
+        return BodySelectCourse();
+    }
+    return Text("hellllloooo");
+  }
+
+  Widget _buildStartButton() {
+    return Material(
+      color: MyColors.courseDetailOrange,
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            _stage = _stage == _details ? _selectCourse : _details;
+          });
+        },
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "HEFJA LEIK",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
